8e24617b389c8606b45584e7ce1447b0806faff8
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-04-11 13:19:05 +0200

    simple search from a list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Users } from "./users";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [query, setQuery] = useState("");
+
+   return (
+      <div className="App">
+         <input
+            type="text"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+         />
+
+         <ul className="list">
+            {Users.filter((item) =>
+               item.first_name.toLowerCase().includes(query.toLowerCase())
+            ).map((person) => (
+               <li className="item" key={person.id}>
+                  {person.first_name}
+               </li>
+            ))}
+         </ul>
+      </div>
+   );
 }
 
 export default App;
